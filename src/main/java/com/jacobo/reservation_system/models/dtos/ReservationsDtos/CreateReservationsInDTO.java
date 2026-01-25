7b5b53c12be045b5ed45ae4caabe50392853f9af
JacoboAP1 <jacobo.arroyavep@autonoma.edu.co>
2026-01-25 15:06:21 -0500

    Deleting unnecessary imports, libraries. And moving docker files to another directory to organize it

--- src/main/java/com/jacobo/reservation_system/models/dtos/ReservationsDtos/CreateReservationsInDTO.java
+++ src/main/java/com/jacobo/reservation_system/models/dtos/ReservationsDtos/CreateReservationsInDTO.java
@@ -1,6 +1,5 @@
 package com.jacobo.reservation_system.models.dtos.ReservationsDtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.time.LocalDate;
--- src/main/java/com/jacobo/reservation_system/repositories/ResourcesRepository.java
+++ src/main/java/com/jacobo/reservation_system/repositories/ResourcesRepository.java
@@ -1,7 +1,6 @@
 package com.jacobo.reservation_system.repositories;
 
 import com.jacobo.reservation_system.models.entities.Resources;
-import com.jacobo.reservation_system.models.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
